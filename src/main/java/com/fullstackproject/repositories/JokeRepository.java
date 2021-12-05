d5c83e42652798478748681eba1f2a2de2738447
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-12-05 17:32:25 +0200

    React, Spring, MySQL

--- src/main/java/com/fullstackproject/repositories/JokeRepository.java
+++ src/main/java/com/fullstackproject/repositories/JokeRepository.java
@@ -4,11 +4,9 @@ import com.fullstackproject.models.Joke;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.concurrent.Future;
 
 @Repository
 public interface JokeRepository extends JpaRepository<Joke, String> {
