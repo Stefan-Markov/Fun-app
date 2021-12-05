a4d368bd09b06f8fb3b24b27f32f3b24be57ab9d
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-12-05 20:21:20 +0200

    React, Spring, MySQL

--- frontend/src/api/contexts/AuthContext.js
+++ frontend/src/api/contexts/AuthContext.js
@@ -1,3 +1,17 @@
-import { createContext } from 'react';
-
-export const AuthContext = createContext();
+// import {createContext, useContext, useState} from "react";
+//
+// export const AuthContext = useContext();
+// //
+// const state = {
+//     authenticatedUser: '',
+//     token: '',
+//     roles: ''
+// }
+//
+// export const AuthProvider = ({children}) => {
+//     const [user, setUser] = useState();
+//
+//     return (<AuthContext.Provider values={{user}}>
+//         {children}
+//     </AuthContext.Provider>);
+// }
