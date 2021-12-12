89fe8a941825a2b00cab6b62885f4affd9963870
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-12-12 23:43:28 +0200

    React, Spring, MySQL

--- frontend/src/components/Account/Account.js
+++ frontend/src/components/Account/Account.js
@@ -1,22 +1,25 @@
 import {getUserByUsername} from "../../api/service/UserService";
 import {useEffect, useState} from "react";
 import './accountStyle.css'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {deleteFavJokeByUsernameAndId, getFavouritesJokeByUsername} from "../../api/service/JokeService";
 import JokeFav from "./JokeFav";
 
 const Account = () => {
-
+    let navigate = useNavigate();
     let username = sessionStorage.getItem('authenticatedUser');
     let [user, setUser] = useState([]);
     let [date, setDate] = useState([]);
     let [roles, setRoles] = useState([]);
     let [favouritesJoke, setFavouritesJoke] = useState([]);
+    let [names, setNames] = useState({username: '', user: ''})
     useEffect(() => {
         getUserByUsername(username)
             .then(res => res.json())
             .then(data => {
                 setUser(data)
+                setNames({username: username, user: data.username})
+
                 setRoles(data.authorities.map(x => x.authority + " "));
                 let date = data.createdDate[0] + "-" + data.createdDate[1] + "-" + data.createdDate[2];
 
@@ -49,16 +52,28 @@ const Account = () => {
     let usernameData = user.username;
     let email = user.email;
 
+    useEffect(() => {
+        let owner = names.username === names.user;
+        if (!owner) {
+            navigate('/');
+        }
+    }, [names, navigate])
+
     return (
         <div className={'wrap-main'}>
             <h1>{usernameData}'s account</h1>
             <div className={'profile'}>
-                <p className={'futura'}><i className="fas fa-user"></i><span className={'account-data'}> Username:</span> {usernameData}</p>
-                <p className={'futura'}>    <i className="fas fa-envelope-open"></i><span className={'account-data'}> Email:</span> {email}</p>
-                <p className={'futura'}><i className="far fa-address-book"></i><span className={'account-data'}> User ID:</span> {user.id}</p>
+                <p className={'futura'}><i className="fas fa-user"></i><span
+                    className={'account-data'}> Username:</span> {usernameData}</p>
+                <p className={'futura'}><i className="fas fa-envelope-open"></i><span
+                    className={'account-data'}> Email:</span> {email}</p>
+                <p className={'futura'}><i className="far fa-address-book"></i><span className={'account-data'}> User ID:</span> {user.id}
+                </p>
                 {date === 'Invalid date' ? '' :
-                    <p className={'futura'}><i className="far fa-calendar-alt"></i><span className={'account-data'}> Date joined:</span> {date}</p>}
-                <p className={'futura'}><i className="fas fa-user-shield"></i><span className={'account-data'}>Roles:</span> {roles}</p>
+                    <p className={'futura'}><i className="far fa-calendar-alt"></i><span className={'account-data'}> Date joined:</span> {date}
+                    </p>}
+                <p className={'futura'}><i className="fas fa-user-shield"></i><span
+                    className={'account-data'}>Roles:</span> {roles}</p>
             </div>
             <div className={'div-buttons'}>
                 <Link className={'link pad'} to={'/joke-add'}>Create joke here</Link>
